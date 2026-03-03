b6388d583b6b568d2bb78c42f46d8ab6788d956d
aceleradora-la <info@aceleradora.la>
2026-03-03 10:45:22 -0300

    Add IVA Exento export extension for l10n_ar_edi_ux.
    Allow AR exempt companies to use export document type 19 for foreign partners and keep WSFEX request handling compatible.

---
+++ l10n_ar_edi_ux_iva_exento_export/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "l10n_ar_edi_ux IVA Exento Export",
+    "version": "19.0.1.0",
+    "category": "Accounting/Localization",
+    "summary": "Allow export document type 19 for exempt companies",
+    "license": "AGPL-3",
+    "author": "ADHOC SA",
+    "website": "https://www.adhoc.com.ar",
+    "depends": ["l10n_ar_edi_ux"],
+    "data": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
